e32d699a627f9aaf3fced4d2de70ef3ad91fbd32
mick309 <pyon3wan2nya1@gmail.com>
2024-12-05 01:33:16 +0900

    20241204oldフォルダ削除

--- old/src/main/java/com/example/tabelog/repository/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tabelog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tabelog.entity.VerificationToken;
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer> {
-	public VerificationToken findByToken(String token);
-}
